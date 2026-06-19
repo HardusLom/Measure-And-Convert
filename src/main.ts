94653281b35e75e3edf51c2b94a20beb5e3197c3
Hardus Lombaard <h.lombaard@cware.co.za>
2026-06-19 12:51:44 +0200

    feat: implement versioning script and update environment handling

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { provideServiceWorker } from '@angular/service-worker';
 import { isDevMode } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
+import { environment } from './environments/environment';
+
+console.log(`Measure & Convert v${environment.version}`);
 
 bootstrapApplication(AppComponent, {
   providers: [
